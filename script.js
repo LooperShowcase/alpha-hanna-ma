52ff73661d9393cf28df40e0ac76995a878b3b3d
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2024-01-23 14:23:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    window.location.href = "iPhone15.html";
+    }
+
+    function mac(){
+        window.location.href = "Mac.html";
+    }
+    function ipad(){
+        window.location.href = "ipad.html";
+    }
